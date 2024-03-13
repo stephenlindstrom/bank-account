8777c1afbdd85fbc2e016edca6078f6e9ca1bcba
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-03-13 15:51:53 -0700

    Update Account class constructor parameters
    Include first name, last name, and pin number as parameters.
    Add unique account number for each Account object that is
    counted using account_no_counter class attribute

--- bank-account.py
+++ bank-account.py
@@ -1,6 +1,13 @@
 class Account:
-    def __init__(self, balance=0):
+    account_no_counter = 0
+
+    def __init__(self, first_name, last_name, pin_no, balance=0):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.pin_no= pin_no
         self.balance = balance
+        Account.account_no_counter +=1 
+        self.account_no = Account.account_no_counter
 
     def deposit(self, deposit_amount):
         self.balance = self.balance + deposit_amount
@@ -11,7 +18,9 @@ class Account:
         else:
             raise Exception("Not enough funds")
 
-account_1 = Account()
+account_1 = Account("Stephen", "Lindstrom", 1234)
+account_2 = Account("John", "Doe", 1111)
 account_1.deposit(100)
-account_1.withdraw(150)
-print(account_1.balance)
+account_1.withdraw(50)
+print(account_1.account_no)
+print(account_2.account_no)
