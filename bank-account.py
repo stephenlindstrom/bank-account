71d077aef7a49bb8935355268722d5d4ee938c9d
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-03-13 15:33:16 -0700

    Create withdraw method in Account class

--- bank-account.py
+++ bank-account.py
@@ -4,3 +4,12 @@ class Account:
 
     def deposit(self, deposit_amount):
         self.balance = self.balance + deposit_amount
+
+    def withdraw(self, withdraw_amount):
+        self.balance = self.balance - withdraw_amount
+
+
+account_1 = Account()
+account_1.deposit(100)
+account_1.withdraw(50)
+print(account_1.balance)
