0c0fafbe5c32360fb266bf6dd6a5fe3d29892443
Stephen Lindstrom <stphnlindstrom@gmail.com>
2024-03-24 19:25:52 -0700

    Add functions to deposit money into existing account

--- bank-account.py
+++ bank-account.py
@@ -3,15 +3,12 @@ import time
 import psycopg2
 
 class Account:
-    account_no_counter = 0
-
-    def __init__(self, first_name, last_name, pin_no, balance=0):
+    def __init__(self, first_name, last_name, pin_no, balance=0, id=0):
         self.first_name = first_name
         self.last_name = last_name
         self.pin_no= pin_no
         self.balance = balance
-        Account.account_no_counter +=1 
-        self.account_no = Account.account_no_counter
+        self.id = id
 
     def deposit(self, deposit_amount):
         self.balance = self.balance + deposit_amount
@@ -26,6 +23,10 @@ def display_home_screen():
     print("1. Create new bank account")
     print("2. Deposit money")
     print("3. Withdraw money")
+
+NEW_ACCOUNT = 1
+DEPOSIT = 2
+WITHDRAW = 3
 
 def get_account_owner_info():
     first_name = input("Please enter first name: ")
@@ -63,28 +64,52 @@ def account_exists(account_no):
     cur.close()
     conn.close()
 
-    print(account_record)
+    if account_record == None:
+        return False
+    else:
+        return True
 
+def create_account_object(account_no):
+    conn = psycopg2.connect(host="127.0.0.1", dbname="bank", user="postgres", password="hello", port=5432)
+
+    cur = conn.cursor()
+    
+    cur.execute("SELECT * FROM account WHERE id=%s;", (account_no))
+    account_record = cur.fetchone()
+
+    conn.commit()
+    cur.close()
+    conn.close()
+    account = Account(account_record[1], account_record[2], account_record[3], account_record[4], account_record[0])
+    return account
+
+def validate_pin_no(account):
+    pin_no = input("Please provide pin number: ")
+    if pin_no == account.pin_no:
+        return True
+    else:
+        return False
 
 def get_deposit_amount():
-    deposit_amount = input("Please enter deposit amount: ")
+    deposit_amount = int(input("Please enter deposit amount: "))
     return deposit_amount
 
-def deposit_money():
-    account_no = input("Please enter account number: ")
-    with open("bank_accounts.csv", "r") as accounts_file:
-        csvreader = csv.reader(accounts_file)
-        for account_entry in csvreader:
-            if account_entry[0] == account_no:
-                account = Account(account_entry[1], account_entry[2], account_entry[3], int(account_entry[4]))
-                print("Account found")
-                break
+def deposit_to_account(account, deposit_amount):
+    account.deposit(deposit_amount)
 
-    with open("bank_accounts.csv", "w") as accounts_file:
-        csvwriter = csv.writer(accounts_file)
-        deposit_amount = int(input("Please enter deposit amount: "))
-        account.deposit(deposit_amount)
-        print(f"Money deposited. New balance is {account.balance}")
+    conn = psycopg2.connect(host="127.0.0.1", dbname="bank", user="postgres", password="hello", port=5432)
+
+    cur = conn.cursor()
+
+    cur.execute("""
+                UPDATE account 
+                SET balance = %s
+                WHERE id = %s;
+                """, (account.balance, account.id))
+    conn.commit()
+
+    cur.close()
+    conn.close()
 
 
 def withdraw_money():
@@ -95,22 +120,22 @@ def withdraw_money():
 def main():
     display_home_screen()
     home_screen_input = int(input("Please select option from above: "))
-    if home_screen_input == 1:
+    if home_screen_input == NEW_ACCOUNT:
         account = get_account_owner_info()
         add_account_to_database(account)
 
-    if home_screen_input == 2:
+    if home_screen_input == DEPOSIT:
         account_no = get_account_no()
-        account_exists(account_no)
+        if account_exists(account_no):
+            account = create_account_object(account_no)
+            if validate_pin_no(account):
+                deposit_amount = get_deposit_amount()
+                deposit_to_account(account, deposit_amount)
+            else:
+                print("Invalid pin number")
+        else:
+            print("Account does not exist")
 
 
 if __name__ == "__main__":
     main()
-"""
-account_1 = Account("Stephen", "Lindstrom", 1234)
-account_2 = Account("John", "Doe", 1111)
-account_1.deposit(100)
-account_1.withdraw(50)
-print(account_1.account_no)
-print(account_2.account_no)
-"""
